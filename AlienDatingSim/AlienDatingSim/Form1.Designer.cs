a869567b7322a95c5638fa006ff7acc82410b312
Glenhawk-0 <LennonEhare@Gmail.com>
2024-12-07 17:46:52 -0500

    Ive created the Base form of the tree node system
    it very much subject to change.
    
    also ive made an array of objects called LoveData. it will be used for the treeNodes eventually. for now though simply know to put alien names, and planet names in there.
    
    also added a media folder for images

--- AlienDatingSim/AlienDatingSim/Form1.Designer.cs
+++ AlienDatingSim/AlienDatingSim/Form1.Designer.cs
@@ -28,90 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOption1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.lblTextBox = new System.Windows.Forms.Label();
+            this.btnOption2 = new System.Windows.Forms.Button();
+            this.btnOption3 = new System.Windows.Forms.Button();
+            this.btnOption4 = new System.Windows.Forms.Button();
+            this.btnStartGame = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnOption1
             // 
-            this.button1.Location = new System.Drawing.Point(40, 355);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 150);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnOption1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOption1.Location = new System.Drawing.Point(120, 355);
+            this.btnOption1.Name = "btnOption1";
+            this.btnOption1.Size = new System.Drawing.Size(150, 150);
+            this.btnOption1.TabIndex = 0;
+            this.btnOption1.Text = "btnOption1";
+            this.btnOption1.UseVisualStyleBackColor = true;
+            this.btnOption1.Visible = false;
+            this.btnOption1.Click += new System.EventHandler(this.btnOption1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(127, 12);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(239, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(353, 272);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // lblTextBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(505, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lblTextBox.AutoSize = true;
+            this.lblTextBox.Location = new System.Drawing.Point(391, 307);
+            this.lblTextBox.Name = "lblTextBox";
+            this.lblTextBox.Size = new System.Drawing.Size(56, 13);
+            this.lblTextBox.TabIndex = 2;
+            this.lblTextBox.Text = "lblTextBox";
             // 
-            // button2
+            // btnOption2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 355);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 150);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnOption2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOption2.Location = new System.Drawing.Point(276, 355);
+            this.btnOption2.Name = "btnOption2";
+            this.btnOption2.Size = new System.Drawing.Size(150, 150);
+            this.btnOption2.TabIndex = 3;
+            this.btnOption2.Text = "btnOption2";
+            this.btnOption2.UseVisualStyleBackColor = true;
+            this.btnOption2.Visible = false;
+            this.btnOption2.Click += new System.EventHandler(this.btnOption2_Click);
             // 
-            // button3
+            // btnOption3
             // 
-            this.button3.Location = new System.Drawing.Point(352, 355);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 150);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnOption3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOption3.Location = new System.Drawing.Point(432, 355);
+            this.btnOption3.Name = "btnOption3";
+            this.btnOption3.Size = new System.Drawing.Size(150, 150);
+            this.btnOption3.TabIndex = 4;
+            this.btnOption3.Text = "btnOption3";
+            this.btnOption3.UseVisualStyleBackColor = true;
+            this.btnOption3.Visible = false;
+            this.btnOption3.Click += new System.EventHandler(this.btnOption3_Click);
             // 
-            // button4
+            // btnOption4
             // 
-            this.button4.Location = new System.Drawing.Point(508, 355);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 150);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnOption4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOption4.Location = new System.Drawing.Point(588, 355);
+            this.btnOption4.Name = "btnOption4";
+            this.btnOption4.Size = new System.Drawing.Size(150, 150);
+            this.btnOption4.TabIndex = 5;
+            this.btnOption4.Text = "btnOption4";
+            this.btnOption4.UseVisualStyleBackColor = true;
+            this.btnOption4.Visible = false;
+            this.btnOption4.Click += new System.EventHandler(this.btnOption4_Click);
             // 
-            // button5
+            // btnStartGame
             // 
-            this.button5.Location = new System.Drawing.Point(664, 355);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 150);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnStartGame.Location = new System.Drawing.Point(12, 12);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(194, 43);
+            this.btnStartGame.TabIndex = 6;
+            this.btnStartGame.Text = "btnStartGamebtnStartGame";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(859, 517);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.btnOption4);
+            this.Controls.Add(this.btnOption3);
+            this.Controls.Add(this.btnOption2);
+            this.Controls.Add(this.lblTextBox);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOption1);
             this.Name = "Form1";
             this.Text = "HoneyMoon";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -123,13 +139,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOption1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lblTextBox;
+        private System.Windows.Forms.Button btnOption2;
+        private System.Windows.Forms.Button btnOption3;
+        private System.Windows.Forms.Button btnOption4;
+        private System.Windows.Forms.Button btnStartGame;
     }
 }
 
--- AlienDatingSim/AlienDatingSim/Form1.cs
+++ AlienDatingSim/AlienDatingSim/Form1.cs
@@ -10,34 +10,380 @@ using System.Windows.Forms;
 
 namespace AlienDatingSim
 {
-    
+
     public partial class Form1 : Form
     {
-        
-        //Dont touch vvvvvvv
+        // Define the root of the decision tree
+        private TreeNode root;
+        private TreeNode currentNode;
+        private bool isEndGame; // Flag to track when the game is at the end
+
+        // Array of LoveData objects
+        private LoveData[] loveDataArray = new LoveData[]
+        {   //just a mockUp
+            new LoveData("Mars", new string[] { "Zorblatt", "Xanthe" }, new string[] { "ZorblattImageLink", "XantheImageLink", "MarsImageLink" }, 1),
+            new LoveData("Venus", new string[] { "Gravlox", "Glaxor" }, new string[] { "GravloxImageLink", "GlaxorImageLink", "VenusImageLink" }, 2),
+            new LoveData("Earth", new string[] { "Luna", "Klara" }, new string[] { "LunaImageLink", "KlaraImageLink", "EarthImageLink" }, 3),
+            new LoveData("Jupiter", new string[] { "Ogar", "Zelda" }, new string[] { "OgarImageLink", "ZeldaImageLink", "JupiterImageLink" }, 4)
+        };
+
+        // Constructor for the Form
+        //  -- Dont touch vvvvvvv
         public Form1()
         {
             InitializeComponent();
+            InitializeTree(); // Initialize the tree once
+        }//  -- Dont touch ^^^^^^
+
+        // TreeNode class to represent the structure of the game
+        class TreeNode
+        {
+            public string QuestionOrAnswer { get; set; }
+            public TreeNode Path1 { get; set; }
+            public TreeNode Path2 { get; set; }
+            public TreeNode Path3 { get; set; }
+            public TreeNode Path4 { get; set; }
+
+            public TreeNode(string questionOrAnswer)
+            {
+                QuestionOrAnswer = questionOrAnswer;
+                Path1 = null;
+                Path2 = null;
+                Path3 = null;
+                Path4 = null;
+            }
         }
-        //Dont touch ^^^^^^^^^
 
-        //Global Varibles
+        // Initialize the game tree with generic questions and answers
+        private void InitializeTree()
+        {
+            root = new TreeNode("Root Node");
 
+            // Branches for the root node
+            root.Path1 = new TreeNode("Node 1");
+            root.Path2 = new TreeNode("Node 2");
+            root.Path3 = new TreeNode("Node 3");
+            root.Path4 = new TreeNode("Node 4");
 
+            // Define further branching from Path1
+            root.Path1.Path1 = new TreeNode("Node 1.1");
+            root.Path1.Path2 = new TreeNode("Node 1.2");
+            root.Path1.Path3 = new TreeNode("Node 1.3");
+            root.Path1.Path4 = new TreeNode("Node 1.4");
 
+            // Add additional branching under Node 1.1
+            root.Path1.Path1.Path1 = new TreeNode("Node 1.1.1");
+            root.Path1.Path1.Path2 = new TreeNode("Node 1.1.2");
+            root.Path1.Path1.Path3 = new TreeNode("Node 1.1.3");
+            root.Path1.Path1.Path4 = new TreeNode("Node 1.1.4");
 
+            // Add additional branching under Node 1.2
+            root.Path1.Path2.Path1 = new TreeNode("Node 1.2.1");
+            root.Path1.Path2.Path2 = new TreeNode("Node 1.2.2");
+            root.Path1.Path2.Path3 = new TreeNode("Node 1.2.3");
+            root.Path1.Path2.Path4 = new TreeNode("Node 1.2.4");
 
+            // Add additional branching under Node 1.3
+            root.Path1.Path3.Path1 = new TreeNode("Node 1.3.1");
+            root.Path1.Path3.Path2 = new TreeNode("Node 1.3.2");
+            root.Path1.Path3.Path3 = new TreeNode("Node 1.3.3");
+            root.Path1.Path3.Path4 = new TreeNode("Node 1.3.4");
 
-        //We may not use this vvvvv
+            // Add additional branching under Node 1.4
+            root.Path1.Path4.Path1 = new TreeNode("Node 1.4.1");
+            root.Path1.Path4.Path2 = new TreeNode("Node 1.4.2");
+            root.Path1.Path4.Path3 = new TreeNode("Node 1.4.3");
+            root.Path1.Path4.Path4 = new TreeNode("Node 1.4.4");
+
+            // Define further branching from Path2
+            root.Path2.Path1 = new TreeNode("Node 2.1");
+            root.Path2.Path2 = new TreeNode("Node 2.2");
+            root.Path2.Path3 = new TreeNode("Node 2.3");
+            root.Path2.Path4 = new TreeNode("Node 2.4");
+
+            // Add additional branching under Node 2.1
+            root.Path2.Path1.Path1 = new TreeNode("Node 2.1.1");
+            root.Path2.Path1.Path2 = new TreeNode("Node 2.1.2");
+            root.Path2.Path1.Path3 = new TreeNode("Node 2.1.3");
+            root.Path2.Path1.Path4 = new TreeNode("Node 2.1.4");
+
+            // Add additional branching under Node 2.2
+            root.Path2.Path2.Path1 = new TreeNode("Node 2.2.1");
+            root.Path2.Path2.Path2 = new TreeNode("Node 2.2.2");
+            root.Path2.Path2.Path3 = new TreeNode("Node 2.2.3");
+            root.Path2.Path2.Path4 = new TreeNode("Node 2.2.4");
+
+            // Add additional branching under Node 2.3
+            root.Path2.Path3.Path1 = new TreeNode("Node 2.3.1");
+            root.Path2.Path3.Path2 = new TreeNode("Node 2.3.2");
+            root.Path2.Path3.Path3 = new TreeNode("Node 2.3.3");
+            root.Path2.Path3.Path4 = new TreeNode("Node 2.3.4");
+
+            // Add additional branching under Node 2.4
+            root.Path2.Path4.Path1 = new TreeNode("Node 2.4.1");
+            root.Path2.Path4.Path2 = new TreeNode("Node 2.4.2");
+            root.Path2.Path4.Path3 = new TreeNode("Node 2.4.3");
+            root.Path2.Path4.Path4 = new TreeNode("Node 2.4.4");
+
+            // Define further branching from Path3
+            root.Path3.Path1 = new TreeNode("Node 3.1");
+            root.Path3.Path2 = new TreeNode("Node 3.2");
+            root.Path3.Path3 = new TreeNode("Node 3.3");
+            root.Path3.Path4 = new TreeNode("Node 3.4");
+
+            // Add additional branching under Node 3.1
+            root.Path3.Path1.Path1 = new TreeNode("Node 3.1.1");
+            root.Path3.Path1.Path2 = new TreeNode("Node 3.1.2");
+            root.Path3.Path1.Path3 = new TreeNode("Node 3.1.3");
+            root.Path3.Path1.Path4 = new TreeNode("Node 3.1.4");
+
+            // Add additional branching under Node 3.2
+            root.Path3.Path2.Path1 = new TreeNode("Node 3.2.1");
+            root.Path3.Path2.Path2 = new TreeNode("Node 3.2.2");
+            root.Path3.Path2.Path3 = new TreeNode("Node 3.2.3");
+            root.Path3.Path2.Path4 = new TreeNode("Node 3.2.4");
+
+            // Add additional branching under Node 3.3
+            root.Path3.Path3.Path1 = new TreeNode("Node 3.3.1");
+            root.Path3.Path3.Path2 = new TreeNode("Node 3.3.2");
+            root.Path3.Path3.Path3 = new TreeNode("Node 3.3.3");
+            root.Path3.Path3.Path4 = new TreeNode("Node 3.3.4");
+
+            // Add additional branching under Node 3.4
+            root.Path3.Path4.Path1 = new TreeNode("Node 3.4.1");
+            root.Path3.Path4.Path2 = new TreeNode("Node 3.4.2");
+            root.Path3.Path4.Path3 = new TreeNode("Node 3.4.3");
+            root.Path3.Path4.Path4 = new TreeNode("Node 3.4.4");
+
+            // Define further branching from Path4
+            root.Path4.Path1 = new TreeNode("Node 4.1");
+            root.Path4.Path2 = new TreeNode("Node 4.2");
+            root.Path4.Path3 = new TreeNode("Node 4.3");
+            root.Path4.Path4 = new TreeNode("Node 4.4");
+
+            // Add additional branching under Node 4.1
+            root.Path4.Path1.Path1 = new TreeNode("Node 4.1.1");
+            root.Path4.Path1.Path2 = new TreeNode("Node 4.1.2");
+            root.Path4.Path1.Path3 = new TreeNode("Node 4.1.3");
+            root.Path4.Path1.Path4 = new TreeNode("Node 4.1.4");
+
+            // Add additional branching under Node 4.2
+            root.Path4.Path2.Path1 = new TreeNode("Node 4.2.1");
+            root.Path4.Path2.Path2 = new TreeNode("Node 4.2.2");
+            root.Path4.Path2.Path3 = new TreeNode("Node 4.2.3");
+            root.Path4.Path2.Path4 = new TreeNode("Node 4.2.4");
+
+            // Add additional branching under Node 4.3
+            root.Path4.Path3.Path1 = new TreeNode("Node 4.3.1");
+            root.Path4.Path3.Path2 = new TreeNode("Node 4.3.2");
+            root.Path4.Path3.Path3 = new TreeNode("Node 4.3.3");
+            root.Path4.Path3.Path4 = new TreeNode("Node 4.3.4");
+
+            // Add additional branching under Node 4.4
+            root.Path4.Path4.Path1 = new TreeNode("Node 4.4.1");
+            root.Path4.Path4.Path2 = new TreeNode("Node 4.4.2");
+            root.Path4.Path4.Path3 = new TreeNode("Node 4.4.3");
+            root.Path4.Path4.Path4 = new TreeNode("Node 4.4.4");
+        }
+
+        // Button click event to start a new game
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            currentNode = root; // Always start from the root
+            isEndGame = false; // Reset the end game flag
+
+            btnStartGame.Visible = false; // Hide start button
+            lblTextBox.Text = $"Node: {currentNode.QuestionOrAnswer}"; // Show the first node
+
+            // Show the options (buttons)
+            UpdateButtonText();
+
+            btnOption1.Visible = true;
+            btnOption2.Visible = true;
+            btnOption3.Visible = true;
+            btnOption4.Visible = true;
+
+            // Reset event handlers for buttons
+            ResetButtonEvents();
+        }
+
+        // Update button text based on current node
+        private void UpdateButtonText()
+        {
+            btnOption1.Text = currentNode.Path1?.QuestionOrAnswer ?? "No Path";
+            btnOption2.Text = currentNode.Path2?.QuestionOrAnswer ?? "No Path";
+            btnOption3.Text = currentNode.Path3?.QuestionOrAnswer ?? "No Path";
+            btnOption4.Text = currentNode.Path4?.QuestionOrAnswer ?? "No Path";
+        }
+
+        // Button click event for Path 1 response
+        private void btnOption1_Click(object sender, EventArgs e)
+        {
+            if (isEndGame)
+            {
+                HandleEndGame();
+            }
+            else
+            {
+                if (currentNode.Path1 != null)
+                {
+                    currentNode = currentNode.Path1; // Move to Path1 branch
+                    PlayGame();
+                }
+                else
+                {
+                    EndGame($"You reached: {currentNode.QuestionOrAnswer}. Game Over!");
+                    isEndGame = true; // Set the game to end
+                }
+            }
+        }
+
+        // Button click event for Path 2 response
+        private void btnOption2_Click(object sender, EventArgs e)
+        {
+            if (isEndGame)
+            {
+                HandleEndGame();
+            }
+            else
+            {
+                if (currentNode.Path2 != null)
+                {
+                    currentNode = currentNode.Path2; // Move to Path2 branch
+                    PlayGame();
+                }
+                else
+                {
+                    EndGame($"You reached: {currentNode.QuestionOrAnswer}. Game Over!");
+                    isEndGame = true; // Set the game to end
+                }
+            }
+        }
+
+        // Button click event for Path 3 response
+        private void btnOption3_Click(object sender, EventArgs e)
+        {
+            if (isEndGame)
+            {
+                HandleEndGame();
+            }
+            else
+            {
+                if (currentNode.Path3 != null)
+                {
+                    currentNode = currentNode.Path3; // Move to Path3 branch
+                    PlayGame();
+                }
+                else
+                {
+                    EndGame($"You reached: {currentNode.QuestionOrAnswer}. Game Over!");
+                    isEndGame = true; // Set the game to end
+                }
+            }
+        }
+
+        // Button click event for Path 4 response
+        private void btnOption4_Click(object sender, EventArgs e)
+        {
+            if (isEndGame)
+            {
+                HandleEndGame();
+            }
+            else
+            {
+                if (currentNode.Path4 != null)
+                {
+                    currentNode = currentNode.Path4; // Move to Path4 branch
+                    PlayGame();
+                }
+                else
+                {
+                    EndGame($"You reached: {currentNode.QuestionOrAnswer}. Game Over!");
+                    isEndGame = true; // Set the game to end
+                }
+            }
+        }
+
+        // Handle the final end game response (no more "correct/incorrect" prompts)
+        private void HandleEndGame()
+        {
+            lblTextBox.Text = $"Game Over! You reached: {currentNode.QuestionOrAnswer}";
+
+            // Hide the option buttons at the end of the game
+            ShowAnswerButtons(false);
+
+            // Show the "Start Game" button to allow restarting
+            btnStartGame.Visible = true;
+        }
+
+        // Show or hide the answer buttons
+        private void ShowAnswerButtons(bool visible)
+        {
+            btnOption1.Visible = visible;
+            btnOption2.Visible = visible;
+            btnOption3.Visible = visible;
+            btnOption4.Visible = visible;
+        }
+
+        // Play the game (ask questions)
+        private void PlayGame()
+        {
+            // Show the current node's position and text
+            lblTextBox.Text = $"Node: {currentNode.QuestionOrAnswer}";
+
+            // Update the button text to reflect the next node choices
+            UpdateButtonText();
+
+            if (currentNode.Path1 == null && currentNode.Path2 == null && currentNode.Path3 == null && currentNode.Path4 == null)
+            {
+                // If at a leaf node, game ends
+                EndGame($"You reached: {currentNode.QuestionOrAnswer}. Game Over!");
+                isEndGame = true; // Set the game to end
+            }
+        }
+
+        // End the game and show the final message
+        private void EndGame(string message)
+        {
+            lblTextBox.Text = message;
+        }
+
+        // Reset button event handlers to prevent duplicates
+        private void ResetButtonEvents()
+        {
+            btnOption1.Click -= btnOption1_Click; // Remove previous event handlers
+            btnOption2.Click -= btnOption2_Click;
+            btnOption3.Click -= btnOption3_Click;
+            btnOption4.Click -= btnOption4_Click;
+
+            // Add new event handlers
+            btnOption1.Click += btnOption1_Click;
+            btnOption2.Click += btnOption2_Click;
+            btnOption3.Click += btnOption3_Click;
+            btnOption4.Click += btnOption4_Click;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        //We may not use this ^^^^^  (leave it though)
+    }//end public partial class Form 1 : Form
 
+    // LoveData class with the specified properties
+    public class LoveData
+    {
+        public string Planet { get; set; }
+        public string[] Aliens { get; set; }
+        public string[] ImageLinks { get; set; }
+        public int Value { get; set; }
 
+        public LoveData(string planet, string[] aliens, string[] imageLinks, int value)
+        {
+            Planet = planet;
+            Aliens = aliens;
+            ImageLinks = imageLinks;
+            Value = value;
+        }
+    }//end of Public class  LoveData
 
-
-
-    }//end of partial class Form1 : Form
-}//End 
+}
