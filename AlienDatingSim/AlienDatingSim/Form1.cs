d6244ba4ad07fd974daff0addf60e6bbe01a9b5b
Glenhawk-0 <LennonEhare@Gmail.com>
2024-12-07 10:46:38 -0500

    Created Windows forms app.
    made little mockup layout. will most likely be changed later

---
+++ AlienDatingSim/AlienDatingSim/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlienDatingSim
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        //Dont touch vvvvvvv
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Dont touch ^^^^^^^^^
+
+        //Global Varibles
+
+
+
+
+
+
+        //We may not use this vvvvv
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //We may not use this ^^^^^  (leave it though)
+
+
+
+
+
+    }//end of partial class Form1 : Form
+}//End 
